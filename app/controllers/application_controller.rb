7786ac0bb3aca63b727bee0d4f4b7c7a2176201d
KAZY9 <gi09ty01@gmail.com>
2023-10-15 22:18:15 +0900

    会員登録、会員情報変更、ログイン、パスワードリセット機能の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    private
+
+    def sign_in_required
+        redirect_to login_url unless user_signed_in?
+    end
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [
+          :name, 
+          :birthdate,
+          :sex, 
+          :job
+        ])
+        devise_parameter_sanitizer.permit(:account_update, keys: [
+          :name, 
+          :birthdate,
+          :sex, 
+          :job
+        ])
+      end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,31 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable
+  
+  validates :name, presence: true
+  validate :password_complexity
+
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update(params, *options)
+    clean_up_passwords
+    result
+  end
+
+  private
+
+  def password_complexity
+    if password.present? and not password.match(/\A(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[ -~]{8,32}\z/)
+      errors.add :password, "は半角英数字特殊記号を含め、8〜32文字で設定してください"
+    end
+  end
+end
