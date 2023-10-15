93800d44067a8b75338daec280e5a457e721668d
KAZY9 <gi09ty01@gmail.com>
2023-10-15 22:39:39 +0900

    会員情報登録、会員情報変更、ログイン、パスワードリセット、登録確認メール再送信の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'mainpages#top'
   get '/top', to: 'mainpages#top'
   post '/calculate', to: 'mainpages#calculate'
   get '/results', to: 'mainpages#results'
   post '/suggest_meal', to: 'mainpages#suggest_meal'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations'
+  }
+  devise_scope :user do
+    get '/sign_up', to: 'users/registrations#new'
+    get '/sign_up/complete', to: 'users/registrations#complete'
+    post '/sign_up/complete', to: 'users/registrations#create'
+    get '/forgot', to: 'users/passwords#new'
+    get '/forgot/complete', to: 'users/passwords#complete'
+    get '/mypage/edit', to: 'users/registrations#edit'
+  end
 end
